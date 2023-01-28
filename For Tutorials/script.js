8e8fa530e4601e1ea72bdad7277cc097edd87599
Up40 <57881134+Unknownplanet40@users.noreply.github.com>
2023-01-28 10:53:09 +0800

    Delete For Tutorials directory

--- For Tutorials/script.js
+++
@@ -1,6 +0,0 @@
-function Disable() {
-  document.getElementById("submit").disabled = true;
-  setTimeout(function () {
-    document.getElementById("submit").disabled = false;
-  }, 3000);
-}
--- For Tutorials/time.php
+++
@@ -1,5 +0,0 @@
-<?php session_start();
-session_destroy();
-setcookie('user', 'Block try After 20 Seconds', time() + 20);
-header("location:index.php");
-?>
